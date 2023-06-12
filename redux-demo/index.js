e4673dd3510f8437f73b3ff165ba3e56060995f7
Benjamin Yong <benj_yongzh@hotmail.com>
2023-06-12 21:41:32 +0800

    Starting on multiple reducers next.

--- redux-demo/index.js
+++ redux-demo/index.js
@@ -1,8 +1,12 @@
 // import redux from 'redux'; ES^ style
 const redux = require("redux");
 const createStore = redux.createStore;
+const bindActionCreators = redux.bindActionCreators;
 
 const BUY_CAKE = "BUY_CAKE";
+const RESTOCK_CAKE = "RESTOCK_CAKE";
+const BUY_ICECREAM = "BUY_ICECREAM";
+const RESTOCK_ICECREAM = "RESTOCK_ICECREAM";
 
 //action creator. it only returns an action
 function buyCake() {
@@ -10,12 +14,37 @@ function buyCake() {
   return {
     type: BUY_CAKE, // this type property is a constant string which describes the action
     info: "First redux action", // other properties of the action object are optional
+    payload: 1, // often, additional info/properties in the action is named as payload
+  };
+}
+
+function restockCake(qty = 1) {
+  // an action is simply an object with a type property
+  return {
+    type: RESTOCK_CAKE, // this type property is a constant string which describes the action
+    info: "second redux action", // other properties of the action object are optional
+    payload: qty,
+  };
+}
+
+function buyIceCream() {
+  return {
+    type: BUY_ICECREAM,
+    payload: 1,
+  };
+}
+
+function restockIceCream(qty = 1) {
+  return {
+    type: RESTOCK_ICECREAM,
+    payload: qty,
   };
 }
 
 // reducer = (previousState, action) => newState
 const initialState = {
   numOfCakes: 10,
+  numOfIceCreams: 20, // multiple objects in a state is possible
 };
 
 // standard form of a reducer
@@ -28,6 +57,21 @@ const reducer = (state = initialState, action) => {
       return {
         ...state, // use spread to make a copy of the oldState, then only edit the numOfCakes
         numOfCakes: state.numOfCakes - 1,
+      };
+    case RESTOCK_CAKE:
+      return {
+        ...state,
+        numOfCakes: state.numOfCakes + action.payload, // use action.payload to refer to the action's payload, which can be an argument of the dispatched action
+      };
+    case BUY_ICECREAM:
+      return {
+        ...state,
+        numOfIceCreams: state.numOfIceCreams - 1,
+      };
+    case RESTOCK_ICECREAM:
+      return {
+        ...state,
+        numOfIceCreams: state.numOfIceCreams + action.payload,
       };
     // always have a default action for unaccounted for actions
     default:
@@ -44,8 +88,23 @@ const unsubscribe = store.subscribe(() => {
 }); //capture the subscribed function to be able to unsubscribe later on
 
 //dispatch takes an action as an arg
-store.dispatch(buyCake());
-store.dispatch(buyCake());
-store.dispatch(buyCake()); //multiple dispatches for testing/example sake
+// store.dispatch(buyCake());
+// store.dispatch(buyCake()); //multiple dispatches for testing/example sake
+// store.dispatch(buyCake());
+// store.dispatch(restockCake(3)); //using an input to determine the action's payload
 
+// bindActionCreators allows you to dispatch the action directly from an object
+const actions = bindActionCreators(
+  { buyCake, restockCake, buyIceCream, restockIceCream },
+  store.dispatch
+);
+actions.buyCake();
+actions.buyCake();
+actions.buyCake();
+actions.restockCake(3);
+actions.buyIceCream();
+actions.buyIceCream();
+actions.restockIceCream(2);
 unsubscribe();
+
+console.log("final state: ", store.getState());
